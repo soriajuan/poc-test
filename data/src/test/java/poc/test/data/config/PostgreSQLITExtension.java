a5c763e6a19cbd6a400f0297b8bdd27aa53c9dc2
Juan Soria <soria.juan@gmail.com>
2022-06-02 18:18:23 -0300

    added test containers docker compose module to support custom docker images

--- data/src/test/java/poc/test/data/config/PostgreSQLITExtension.java
+++ data/src/test/java/poc/test/data/config/PostgreSQLITExtension.java
@@ -2,19 +2,28 @@ package poc.test.data.config;
 
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.DockerComposeContainer;
+
+import java.io.File;
 
 public class PostgreSQLITExtension implements BeforeAllCallback {
 
-    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13"));
+    static final String SERVICE_NAME = "postgres";
+    static final Integer SERVICE_PORT = 5432;
+
+    static final DockerComposeContainer<?> environment =
+            new DockerComposeContainer<>(new File("src/test/resources/docker/docker-compose.yml"))
+                    .withExposedService(SERVICE_NAME, SERVICE_PORT);
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
-        postgres.start();
-        System.setProperty("spring.datasource.url", postgres.getJdbcUrl());
-        System.setProperty("spring.datasource.username", postgres.getUsername());
-        System.setProperty("spring.datasource.password", postgres.getPassword());
+        environment.start();
+        var jdbcUrl = String.format("jdbc:postgresql://%s:%d/my_db",
+                environment.getServiceHost(SERVICE_NAME, SERVICE_PORT),
+                environment.getServicePort(SERVICE_NAME, SERVICE_PORT));
+        System.setProperty("spring.datasource.url", jdbcUrl);
+        System.setProperty("spring.datasource.username", "my_user");
+        System.setProperty("spring.datasource.password", "my_user");
     }
 
 }
